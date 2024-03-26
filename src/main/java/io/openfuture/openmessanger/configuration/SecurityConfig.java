b4c5f003cd3bc3729751849b08b4f342e445021f
Beksultan <beksultan@openfuture.io>
2024-03-27 04:14:29 +0600

    OC-91 Spring security

---
+++ src/main/java/io/openfuture/openmessanger/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package io.openfuture.openmessanger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.openfuture.openmessanger.security.AwsCognitoTokenFilter;
+import io.openfuture.openmessanger.security.CognitoAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CognitoAuthenticationProvider cognitoAuthenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(cognitoAuthenticationProvider);
+        final AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/public/login").permitAll()
+                        .requestMatchers("/api/v1/public/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new AwsCognitoTokenFilter("/api/**", authenticationManager,
+                                                           "/api/v1/public/login", "/api/v1/public/signup"),
+                                 UsernamePasswordAuthenticationFilter.class)
+                .authenticationManager(authenticationManager)
+                .build();
+    }
+
+}
---
+++ src/main/java/io/openfuture/openmessanger/domain/User.java
@@ -0,0 +1,12 @@
+package io.openfuture.openmessanger.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+}
