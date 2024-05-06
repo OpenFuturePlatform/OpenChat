cec1c921e5f22044835cf57573326094acd0f338
Beksultan <beksultan@openfuture.io>
2024-05-06 19:50:21 +0600

    OC-124 [back-end] Add Gemini Assistant

--- src/main/java/io/openfuture/openmessanger/assistant/gemini/GeminiHttpConfiguration.java
+++ src/main/java/io/openfuture/openmessanger/assistant/gemini/GeminiHttpConfiguration.java
@@ -26,11 +26,3 @@ public class GeminiHttpConfiguration {
     }
 
 }
-
-
-/*
-curl \
-  -H 'Content-Type: application/json' \
-  -d '{"contents":[{"parts":[{"text":"Write a story about a magic backpack"}]}]}' \
-  -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=YOUR_API_KEY'
-* * */
